6561a46e89ae740da3b8b9691266591606baf2ec
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-08-18 10:40:50 +0600

    Displaying Critic Score

---
+++ src/components/CriticScore.jsx
@@ -0,0 +1,15 @@
+import { Badge } from '@chakra-ui/react';
+import React from 'react';
+
+const CriticScore = ({ score }) => {
+  let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
+  return (
+    <div>
+      <Badge colorScheme={color} fontSize={'14px'} paddingX={2} borderRadius={'4px'}>
+        {score}
+      </Badge>
+    </div>
+  );
+};
+
+export default CriticScore;
